c4780bab213afba0c328f1bc7000e194ab2e2f25
ezen <ezen@DESKTOP-O03MT84>
2021-03-17 17:14:48 +0900

    nbv

---
+++ HIUS/src/main/java/controller/member/MemberController.java
@@ -0,0 +1,12 @@
+package controller.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	@RequestMapping("member/MemRegist")
+	public String MemRegist() {
+	 return "member/memRegist";
+ };
+}
