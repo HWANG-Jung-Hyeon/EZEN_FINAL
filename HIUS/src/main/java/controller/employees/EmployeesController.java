4045e0eb22b25fd864561990dbae93f190965d45
jhy118 <jhyong118@gmail.com>
2021-03-17 16:41:03 +0900

    20210317이노규 04

--- HIUS/src/main/java/controller/employees/EmployeesController.java
+++ HIUS/src/main/java/controller/employees/EmployeesController.java
@@ -1,5 +1,12 @@
 package controller.employees;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class EmployeesController {
-	
+	@RequestMapping("/employees/emp_regist")
+	public String emp_regist() {
+		return "employees/emp_regist";
+	}
 }
