b69447a2fd4325d1526901a0c9d432ebb873618b
ezen <ezen@DESKTOP-O03MT84>
2021-03-18 16:24:48 +0900

    박성도. 오류 제거

--- HIUS/src/main/java/controller/member/MemberController.java
+++ HIUS/src/main/java/controller/member/MemberController.java
@@ -1,20 +1,23 @@
 package controller.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import command.MemberCommand;
-import service.employees.MemberJoinService;
+import service.member.MemberJoinService;
 
 @Controller
 public class MemberController {
+	@Autowired
+	MemberJoinService memberJoinService;
 	@RequestMapping("member/memRegist")
 	public String MemRegist() {
 	 return "member/memRegist";
 	}
 	@RequestMapping("MemberJoinAction")
 	public String memJoin(MemberCommand memberCommand) {
-		MemberJoinService.numupdate(memberCommand);
+		memberJoinService.numUpdate(memberCommand);
 		return "redirect:/member/memRegist";
 	}
 }
