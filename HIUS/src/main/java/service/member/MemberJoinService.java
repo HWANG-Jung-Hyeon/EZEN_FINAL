5d148ae9c473ade6e8b30b85e38a815a623dc5f0
ezen <ezen@DESKTOP-O03MT84>
2021-03-18 16:22:13 +0900

    성도 재 저장

--- HIUS/src/main/java/service/member/MemberJoinService.java
+++ HIUS/src/main/java/service/member/MemberJoinService.java
@@ -9,10 +9,9 @@ import model.MemDTO;
 import repository.member.MemberRepository;
 
 public class MemberJoinService {
-		@Autowired
-		MemberRepository memberRepository;
-		public void numUpdate(MemberCommand memberCommand) {			
-		memberRepository.memInsert(dto);	
+	@Autowired
+	MemberRepository memberRepository;
+	public void numUpdate(MemberCommand memberCommand) {				
 		MemDTO dto = new MemDTO();
 		dto.setMEM_ID(memberCommand.getMEM_ID());
 		dto.setMEM_PW(memberCommand.getMEM_PW());
@@ -22,5 +21,6 @@ public class MemberJoinService {
 		dto.setMEM_ADDR(memberCommand.getMEM_ADDR());
 		dto.setMEM_EMAIL(memberCommand.getMEM_EMAIL());
 		dto.setGENDER(memberCommand.getGENDER());
+		memberRepository.memInsert(dto);
 	}
 }
